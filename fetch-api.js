3c841bc900534c7316e44a7c5f8df431906cc1a0
BENIE SYLVESTRE <sylvestrebenie7829@gmail.com>
2025-03-24 12:58:41 +0000

    feat: exercice CRUD avec axios

---
+++ fetch-api.js
@@ -0,0 +1,43 @@
+// const PLACEHOLDER_URL_API = "https://jsonplaceholder.typicode.com/users"
+
+const userID = 1
+
+// fetch(`${PLACEHOLDER_URL_API}/${userID}`, {
+//   method: "GET",
+// })
+
+// Fetch user data from API
+// fetch(`${PLACEHOLDER_URL_API}`, {
+//   method: "GET",
+// })
+
+// 
+
+// fetch(`${PLACEHOLDER_URL_API}/${userID}`, {
+//   method: 'PUT',
+//   body: JSON.stringify({
+//     name: 'Koffi',
+//     username: 'suits',
+//     email: 'benie@gmail.com'
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+
+
+// fetch(`${PLACEHOLDER_URL_API}/${userID}`, {
+//   method: 'DELETE',
+// });
+
+
+// .then((res)=>{
+//   return res.json();
+// })
+// .then((res) => {
+//   console.log(res);
+// })
+
+// .catch((err) => {
+//   console.error('Error:', err);
+// });
